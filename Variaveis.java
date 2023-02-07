bb17509334160109931cecb49c0645366c195bc3
DudaaMoura <mariamourapalmital@gmail.com>
2023-02-07 15:17:02 +0000

    Salvando as variaveis

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,17 @@
 public class Variaveis{
     
-}
+    public static void main(String[] args){
+
+
+    int idade = 39;
+    char letra = 'M';
+    boolean vivo = false;
+    byte tamanho = 123;
+    double salario = 1302;
+    float salarioMenor = 1272;
+    short alunosPalmital = 10000;
+    long populacaoPalmital = 22272;
+    System.out.println(idade);
+
+
+}}
