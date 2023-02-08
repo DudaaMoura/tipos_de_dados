2ef08fd60f976e1c1807279ca720547a2a7bef29
DudaaMoura <mariamourapalmital@gmail.com>
2023-02-08 17:04:19 +0000

    variaveis salvas

--- Variaveis.java
+++ Variaveis.java
@@ -3,7 +3,8 @@ public class Variaveis{
     public static void main(String[] args){
 
 
-    int idade = 39;
+    int idade = 39 ;
+    byte quantidadedeBolas = 7;
     char letra = 'M';
     boolean vivo = false;
     byte tamanho = 123;
@@ -11,7 +12,18 @@ public class Variaveis{
     float salarioMenor = 1272;
     short alunosPalmital = 10000;
     long populacaoPalmital = 22272;
+    String nome;
+    nome = "Duda";
+    System.out.println(quantidadedeBolas);
+    System.out.println(letra);
     System.out.println(idade);
+    System.out.println(vivo);
+    System.out.println(tamanho);
+    System.out.println(salario);
+    System.out.println(salarioMenor);
+    System.out.println(alunosPalmital);
+    System.out.println(populacaoPalmital);
+    System.out.println("Meu nome é: "+nome + " tenho: "+idade+" anos");
 
 
 }}
