941037dcd703c4537f8820ee63e3143b09e303ef
DudaaMoura <mariamourapalmital@gmail.com>
2023-02-28 14:37:16 +0000

    Salvando ex

---
+++ BalancoTrimestral.java
@@ -0,0 +1,15 @@
+public class BalancoTrimestral {
+    public static void main(String[]Dudinha){
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+        int mediaMensal = gastosTrimestre/3;
+
+        System.out.println("O valor dos gastos trimestrais são: " + gastosTrimestre) ;
+
+        System.out.println("A média mensal é: " + mediaMensal) ;
+
+    }}
+
+        
